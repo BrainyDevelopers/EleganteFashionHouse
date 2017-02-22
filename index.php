fcb338c7998a6f273132e1eb946b58032d9c71e9
Anthony Abeo <anthonyabeo@gmail.com>
2017-02-22 23:13:52 +0000

    index.php footer updated

--- index.php
+++ index.php
@@ -494,52 +494,39 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			</div>
 				<div class="footer-bottom">
 				<div class="container">
-						<div class="col-md-3 footer-bottom-cate animated wow fadeInLeft" data-wow-delay=".5s">
+						<div class="col-md-4 footer-bottom-cate">
 							<h6>Categories</h6>
 							<ul>
-								<li><a href="View/Customer/products.php">Curabitur sapien</a></li>
-								<li><a href="View/Customer/single.php">Dignissim purus</a></li>
-								<li><a href="View/Customer/men.php">Tempus pretium</a></li>
-								<li><a href="View/Customer/products.php">Dignissim neque</a></li>
-								<li><a href="View/Customer/single.php">Ornared id aliquet</a></li>
-								
+								<li><a href="products.php">Men's</a></li>
+								<li><a href="single.php">Women's</a></li>
+								<li><a href="men.php">Shoes</a></li>
+								<li><a href="products.php">Work out</a></li>
+								<li><a href="single.php">Dinner</a></li>	
 							</ul>
 						</div>
-						<div class="col-md-3 footer-bottom-cate animated wow fadeInLeft" data-wow-delay=".5s">
-							<h6>Feature Projects</h6>
-							<ul>
-								<li><a href="View/Customer/products.php">Dignissim purus</a></li>
-								<li><a href="View/Customer/men.php">Curabitur sapien</a></li>
-								<li><a href="View/Customer/single.php">Tempus pretium</a></li>
-								<li><a href="View/Customer/men.php">Dignissim neque</a></li>
-								<li><a href="View/Customer/products.php">Ornared id aliquet</a></li>
-							</ul>
-						</div>
-						<div class="col-md-3 footer-bottom-cate animated wow fadeInRight" data-wow-delay=".5s">
+						
+						<div class="col-md-4 footer-bottom-cate">
 							<h6>Top Brands</h6>
 							<ul>
-								<li><a href="View/Customer/products.php">Tempus pretium</a></li>
-								<li><a href="View/Customer/single.php">Curabitur sapien</a></li>
-								<li><a href="View/Customer/men.php">Dignissim purus</a></li>
-								<li><a href="View/Customer/single.php">Dignissim neque</a></li>
-								<li><a href="View/Customer/men.php">Ornared id aliquet</a></li>
-								
-								
+								<li><a href="products.php">Burburry</a></li>
+								<li><a href="single.php">Hugo Boss</a></li>
+								<li><a href="men.php">G-Star</a></li>
+								<li><a href="single.php">Giorgio Armani</a></li>
+								<li><a href="men.php">Clarks</a></li>
 							</ul>
 						</div>
-						<div class="col-md-3 footer-bottom-cate cate-bottom animated wow fadeInRight" data-wow-delay=".5s">
+						<div class="col-md-4 footer-bottom-cate cate-bottom">
 							<h6>Our Address</h6>
 							<ul>
-								<li><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i>Address : 12th Avenue, 5th block, <span>Sydney.</span></li>
-								<li><i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>Email : <a href="mailto:info@example.com">info@example.com</a></li>
-								<li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i>Phone : +1234 567 567</li>
+								<li><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i>Address : 45th Mango Lane, <span>Accra.</span></li>
+								<li><i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>Email : <a href="mailto:info@example.com">info@fashion.com</a></li>
+								<li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i>Phone : +(233) 302 007 007</li>
 							</ul>
 						</div>
 						<div class="clearfix"> </div>
-						<p class="footer-class animated wow fadeInUp animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;"> © 2016 Youth Fashion . All Rights Reserved | Design by <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+						<p class="footer-class"> © 2016 Youth Fashion . All Rights Reserved | Design by <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
 					</div>
 			</div>
-		</div>
 		<!--footer-->
 	</body>
 </html>
