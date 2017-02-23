1511e8934f761ed2607c4b6a3bc2fc5a204f9339
Ibrahim-Abdullah <braimahabdulla@gmail.com>
2017-02-22 21:34:36 -0800

    Menu.php completed

--- View/Customer/single.php
+++ View/Customer/single.php
@@ -40,109 +40,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 
 </head>
 <body>
-<!--header-->
-<div class="header">
-	<div class="header-top">
-		<div class="container">
-				<div class="col-sm-4 logo">
-					<h1><a href="index.php">Youth <span>Fashion</span></a></h1>	
-				</div>
-			<div class="col-sm-4 world">
-					<div class="cart box_1">
-						<a href="checkout.php">
-						<h3> <div class="total">
-							<span class="simpleCart_total"></span></div>
-							<img src="../images/cart.png" alt=""/></h3>
-						</a>
-						<p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
-
-					</div>
-			</div>
-			<div class="col-sm-2 number">
-					<span><i class="glyphicon glyphicon-phone"></i>085 596 234</span>
-					<p>Call me</p>
-				</div>
-			<div class="col-sm-2 search">		
-				<a class="play-icon popup-with-zoom-anim" href="#small-dialog"><i class="glyphicon glyphicon-search"> </i> </a>
-			</div>
-				<div class="clearfix"> </div>
-		</div>
-	</div>
-		<div class="container">
-			<div class="head-top">
-			<div class="n-avigation">
-			
-				<nav class="navbar nav_bottom" role="navigation">
-				
-				 <!-- Brand and toggle get grouped for better mobile display -->
-				  <div class="navbar-header nav_2">
-					  <button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					  </button>
-					  <a class="navbar-brand" href="#"></a>
-				   </div> 
-				   <!-- Collect the nav links, forms, and other content for toggling -->
-					<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
-						<ul class="nav navbar-nav nav_1">
-							<li><a href="index.php">Home</a></li>
-							<li class="dropdown mega-dropdown active">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Women<span class="caret"></span></a>				
-								<div class="dropdown-menu mega-dropdown-menu">
-									<div class="container-fluid">
-										<!-- Tab panes -->
-										<div class="tab-content">
-										  <div class="tab-pane active" id="men">
-											<ul class="nav-list list-inline">
-												<li><a href="women.php"><img src="../images/t7.jpg" class="img-responsive" alt=""/></a></li>
-												<li><a href="women.php"><img src="../images/t8.jpg" class="img-responsive" alt=""/></a></li>
-												<li><a href="women.php"><img src="../images/t9.jpg" class="img-responsive" alt=""/></a></li>
-												<li><a href="women.php"><img src="../images/t11.jpg" class="img-responsive" alt=""/></a></li>
-												<li><a href="women.php"><img src="../images/t1.jpg" class="img-responsive" alt=""/></a></li>
-												<li><a href="women.php"><img src="../images/t12.jpg" class="img-responsive" alt=""/></a></li>
-											</ul>
-										  </div>
-									   </div>
-									</div>
-									<!-- Nav tabs -->
-									                
-								</div>				
-							</li>
-							<li class="dropdown mega-dropdown active">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>				
-								<div class="dropdown-menu mega-dropdown-menu">
-									<div class="container-fluid">
-										<!-- Tab panes -->
-										<div class="tab-content">
-										  <div class="tab-pane active" id="men">
-											<ul class="nav-list list-inline">
-												<li><a href="men.php"><img src="../images/t10.jpg" class="img-responsive" alt=""/></a></li>
-												<li><a href="men.php"><img src="../images/t2.jpg" class="img-responsive" alt=""/></a></li>
-												<li><a href="men.php"><img src="../images/t3.jpg" class="img-responsive" alt=""/></a></li>
-												<li><a href="men.php"><img src="../images/t4.jpg" class="img-responsive" alt=""/></a></li>
-												<li><a href="men.php"><img src="../images/t5.jpg" class="img-responsive" alt=""/></a></li>
-												<li><a href="men.php"><img src="../images/t6.jpg" class="img-responsive" alt=""/></a></li>
-											</ul>
-										  </div>
-										 
-									   </div>
-									</div>
-									<!-- Nav tabs -->
-									                   
-								</div>				
-							</li>
-							<li><a href="products.php">Products</a></li>
-							<li><a href="account.php">Sign In</a></li>
-							<li class="last"><a href="contact.php">Contact</a></li>
-						</ul>
-					 </div><!-- /.navbar-collapse -->
-				  
-				</nav>
-			</div>
-			
-				
+	<!-- Include menu.php -->
+		<?php require('menu.php');?> 
+		
 		<div class="clearfix"> </div>
 			<!---pop-up-box---->   
 					<link href="../css/popuo-box.css" rel="stylesheet" type="text/css" media="all"/>
@@ -183,11 +83,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <div class="breadcrumbs">
 		<div class="container">
 			<ol class="breadcrumb breadcrumb1 animated wow slideInLeft animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: slideInLeft;">
-				<li><a href="index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
+				<li><a href="../../index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
 				<li class="active">Single</li>
 			</ol>
 		</div>
 	</div>
+
 <div class="single">
 
 <div class="container">
