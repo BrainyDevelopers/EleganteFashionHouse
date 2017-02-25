efcb866445936e00e7b97ebf8f9770d106a26e57
warihanagumah <warihana.gumah@ashesi.edu.gh>
2017-02-25 13:40:43 +0000

    changes to menu

--- index.php
+++ index.php
@@ -45,7 +45,157 @@ Customized by Brainy Developers, developers of e-commerce website for Elegante F
 	</head>
 	<body>
 		<!--header-->
-		<?php require('View/Customer/menu.php'); ?>
+		<div class="header">
+			<div class="header-top">
+				<div class="container">
+						<div class="col-sm-4 logo animated wow fadeInLeft" data-wow-delay=".5s">
+							<h1><a href="index.php">ELEGANTE<span>Fashion</span></a></h1>	
+						</div>
+					<div class="col-sm-4 world animated wow fadeInRight" data-wow-delay=".5s">
+							<div class="cart box_1">
+								<a href="View/Customer/checkout.php">
+								<h3> <div class="total">
+									<span class="simpleCart_total"></span></div>
+									<img src="View/images/cart.png" alt=""/></h3>
+								</a>
+								<p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+
+							</div>
+					</div>
+					<div class="col-sm-2 number animated wow fadeInRight" data-wow-delay=".5s">
+							<span><i class="glyphicon glyphicon-phone"></i>+233 201484933</span>
+							<p>Call me</p>
+						</div>
+					<div class="col-sm-2 search animated wow fadeInRight" data-wow-delay=".5s">		
+						<a class="play-icon popup-with-zoom-anim" href="#small-dialog"><i class="glyphicon glyphicon-search"> </i> </a>
+					</div>
+						<div class="clearfix"> </div>
+				</div>
+			</div>
+				<div class="container">
+					<div class="head-top">
+					<div class="n-avigation">
+					
+						<nav class="navbar nav_bottom" role="navigation">
+						
+						 <!-- Brand and toggle get grouped for better mobile display -->
+						  <div class="navbar-header nav_2">
+							  <button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							  </button>
+							  <a class="navbar-brand" href="#"></a>
+						   </div> 
+						   <!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+								<ul class="nav navbar-nav nav_1">
+									<li><a href="index.php">Home</a></li>
+									<li class="dropdown mega-dropdown active">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">Women<span class="caret"></span></a>				
+										<div class="dropdown-menu mega-dropdown-menu">
+											<div class="container-fluid">
+												<!-- Tab panes -->
+												<div class="tab-content">
+												  <div class="tab-pane active" id="men">
+													<ul class="nav-list list-inline">
+														<li>
+															<a href="View/customer/women-trousers.php"><img src="View/images/t7.jpg" class="img-responsive" alt=""/></a>
+															<p>Trousers</p>
+														</li>
+														<li>
+															<a href="View/customer/women-skirts.php"><img src="View/images/t8.jpg" class="img-responsive" alt=""/></a>
+															<p>Skirts</p>
+														</li>
+														<li>
+															<a href="View/customer/women-shoes.php"><img src="View/images/t9.jpg" class="img-responsive" alt=""/></a>
+															<p>Shoes</p>
+														</li>
+													</ul>
+												  </div>
+											   </div>
+											</div>
+											<!-- Nav tabs -->
+											                
+										</div>				
+									</li>
+									<li class="dropdown mega-dropdown active">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>				
+										<div class="dropdown-menu mega-dropdown-menu">
+											<div class="container-fluid">
+												<!-- Tab panes -->
+												<div class="tab-content">
+												  <div class="tab-pane active" id="men">
+													<ul class="nav-list list-inline">
+														<li>
+															<a href="View/Customer/men-trousers.php"><img src="View/images/t10.jpg" class="img-responsive" alt=""/></a>
+															<p>Trousers</p>
+														</li>
+														<li>
+															<a href="View/Customer/men-shirts.php"><img src="View/images/t2.jpg" class="img-responsive" alt=""/></a>
+															<p>Shirts</p>
+														</li>
+														<li>
+															<a href="View/Customer/men-shoes.php"><img src="View/images/t3.jpg" class="img-responsive" alt=""/></a>
+															<p>Shoes</p>
+														</li>
+													</ul>
+												  </div>
+												 
+											   </div>
+											</div>
+											<!-- Nav tabs -->
+											                   
+										</div>				
+									</li>
+									<li><a href="View/Customer/products.php">Products</a></li>
+									<li><a href="View/Customer/account.php">Sign In</a></li>
+									<li class="last"><a href="View/Customer/contact.php">Contact</a></li>
+								</ul>
+							 </div><!-- /.navbar-collapse -->
+						  
+						</nav>
+					</div>
+					
+						
+				<div class="clearfix"> </div>
+					<!---pop-up-box---->   
+							<link href="View/css/popuo-box.css" rel="stylesheet" type="text/css" media="all"/>
+							<script src="View/js/jquery.magnific-popup.js" type="text/javascript"></script>
+							<!---//pop-up-box---->
+						<div id="small-dialog" class="mfp-hide">
+						<div class="search-top">
+								<div class="login">
+									<form action="#" method="post">
+										<input type="submit" value="">
+										<input type="text" name="search" value="Type something..." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '';}">		
+									
+									</form>
+								</div>
+								<p>	Shopping</p>
+							</div>				
+						</div>
+						 <script>
+								$(document).ready(function() {
+								$('.popup-with-zoom-anim').magnificPopup({
+									type: 'inline',
+									fixedContentPos: false,
+									fixedBgPos: true,
+									overflowY: 'auto',
+									closeBtnInside: true,
+									preloader: false,
+									midClick: true,
+									removalDelay: 300,
+									mainClass: 'my-mfp-zoom-in'
+								});
+																								
+								});
+						</script>			
+			<!---->		
+				</div>
+			</div>
+		</div>
 		<!--banner-->
 		<div class="banner">
 			<div class="matter-banner">
@@ -323,7 +473,8 @@ Customized by Brainy Developers, developers of e-commerce website for Elegante F
 					</div>	
 				</div>
 			</div>
+		<!--footer-->
 		<?php include ('View/Customer/footer.php');?>
-	
+		
 	</body>
 </html>
