b50998c5caea793a0550b1bf0705b0c01e8bba09
Anthony Abeo <anthonyabeo@gmail.com>
2017-02-25 13:30:59 +0000

    manage-inventory page up and running

---
+++ admin/manage-inventory.php
@@ -0,0 +1,256 @@
+<!--A Design by W3layouts 
+Author: W3layout
+Author URL: http://w3layouts.com
+Customized by Brainy Developers, developers of e-commerce website for Elegante Fashion House 
+-->
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Elegante Fashion House - Products</title>
+		<link href="assets/css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="assets/js/jquery.min.js"></script>
+		<!-- Custom Theme files -->
+		<!--theme-style-->
+		<link href="assets/css/style-c.css" rel="stylesheet" type="text/css" media="all" />	
+		<!--//theme-style-->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="keywords" content="Elegante Fashion House, Youth Fashion, Ghana Fashion House, Contemporary Designs, apparels in store" />
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+		<link href='//fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+		<link href='//fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+		<!-- start menu -->
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/simpleCart.min.js"> </script>
+		<!-- slide -->
+		<script src="assets/js/responsiveslides.min.js"></script>
+
+		   <script>
+		    $(function () {
+		      $("#slider").responsiveSlides({
+		      	auto: true,
+		      	speed: 500,
+		        namespace: "callbacks",
+		        pager: true,
+		      });
+		    });
+		  </script>
+	</head>
+	<body>
+
+		<div class="products">
+			<div class="container">
+				<h2>ALL PRODUCTS</h2>
+				<div class="col-md-9">
+					<div class="content-top1">
+						<div class="col-md-4 col-md4">
+							<div class="col-md1 simpleCart_shelfItem">
+								<a href="single.html">
+									<img class="img-responsive" src="../images/pi9.png" alt="" />
+								</a>
+								<h3><a href="single.php">Skirt</a></h3>
+								<div class="price">
+										<h5 class="item_price">$60</h5>
+										<a href="edit-product.php" class="item_add edit">EDIT</a><br>
+										<a href="add-product.php" class="item_add delete">DELETE</a>
+										<div class="clearfix"> </div>
+								</div>
+							</div>
+						</div>	
+					<div class="col-md-4 col-md4">
+							<div class="col-md1 simpleCart_shelfItem">
+								<a href="single.html">
+									<img class="img-responsive" src="../images/pi12.png" alt="" />
+
+								</a>
+								<h3><a href="single.php">Trouser</a></h3>
+								<div class="price">
+										<h5 class="item_price">$60</h5>
+										<a href="edit-product.php" class="item_add edit">EDIT</a><br>
+										<a href="add-product.php" class="item_add delete">DELETE</a>
+										<div class="clearfix"> </div>
+								</div>
+								
+							</div>
+						</div>	
+					<div class="col-md-4 col-md4">
+							<div class="col-md1 simpleCart_shelfItem">
+								<a href="single.html">
+									<img class="img-responsive" src="../images/pi10.png" alt="" />
+
+								</a>
+								<h3><a href="single.php">Pant</a></h3>
+								<div class="price">
+										<h5 class="item_price">$70</h5>
+										<a href="edit-product.php" class="item_add edit">EDIT</a><br>
+										<a href="add-product.php" class="item_add delete">DELETE</a>
+										<div class="clearfix"> </div>
+								</div>
+								
+							</div>
+						</div>	
+					
+					<div class="clearfix"> </div>
+					</div>	
+					<div class="content-top1">
+						<div class="col-md-4 col-md4">
+							<div class="col-md1 simpleCart_shelfItem">
+								<a href="single.html">
+									<img class="img-responsive" src="../images/pi11.png" alt="" />
+
+								</a>
+								<h3><a href="single.php">Trouser</a></h3>
+								<div class="price">
+										<h5 class="item_price">$80</h5>
+										<a href="edit-product.php" class="item_add edit">EDIT</a><br>
+										<a href="add-product.php" class="item_add delete">DELETE</a>
+										<div class="clearfix"> </div>
+								</div>
+							</div>
+						</div>	
+					<div class="col-md-4 col-md4">
+							<div class="col-md1 simpleCart_shelfItem">
+								<a href="single.html">
+									<img class="img-responsive" src="../images/pi9.png" alt="" />
+
+								</a>
+								<h3><a href="single.php">Palazoo</a></h3>
+								<div class="price">
+										<h5 class="item_price">$90</h5>
+										<a href="edit-product.php" class="item_add edit">EDIT</a><br>
+										<a href="add-product.php" class="item_add delete">DELETE</a>
+										<div class="clearfix"> </div>
+								</div>
+								
+							</div>
+						</div>	
+					<div class="col-md-4 col-md4">
+							<div class="col-md1 simpleCart_shelfItem">
+								<a href="single.php">
+									<img class="img-responsive" src="../images/pi12.png" alt="" />
+								</a>
+								<h3><a href="single.php">Palazoo</a></h3>
+								<div class="price">
+									<h5 class="item_price">$85</h5>
+									<a href="edit-product.php" class="item_add edit">EDIT</a><br>
+									<a href="add-product.php" class="item_add delete">DELETE</a>
+									<div class="clearfix"> </div>
+								</div>
+								
+							</div>
+						</div>	
+					
+					<div class="clearfix"> </div>
+					</div>	
+					<div class="content-top1">
+						<div class="col-md-4 col-md4">
+							<div class="col-md1 simpleCart_shelfItem">
+								<a href="single.php">
+									<img class="img-responsive" src="../images/pi10.png" alt="" />
+								</a>
+								<h3><a href="single.php">Trouser</a></h3>
+								<div class="price">
+										<h5 class="item_price">$76</h5>
+										<a href="edit-product.php" class="item_add edit">EDIT</a><br>
+										<a href="add-product.php" class="item_add delete">DELETE</a>
+										<div class="clearfix"> </div>
+								</div>
+							</div>
+						</div>	
+					<div class="col-md-4 col-md4">
+							<div class="col-md1 simpleCart_shelfItem">
+
+								<a href="single.php">
+									<img class="img-responsive" src="../images/pi11.png" alt="" />
+								</a>
+								<h3><a href="single.php">Jeans</a></h3>
+								<div class="price">
+										<h5 class="item_price">$70</h5>
+										<a href="edit-product.php" class="item_add edit">EDIT</a><br>
+										<a href="add-product.php" class="item_add delete">DELETE</a>
+										<div class="clearfix"> </div>
+								</div>
+								
+							</div>
+						</div>	
+					<div class="col-md-4 col-md4">
+							<div class="col-md1 simpleCart_shelfItem">
+								<a href="single.php">
+									<img class="img-responsive" src="../images/pi9.png" alt="" />
+								</a>
+								<h3><a href="single.php">Trouser</a></h3>
+								<div class="price">
+										<h5 class="item_price">$80</h5>
+										<a href="edit-product.php" class="item_add edit">EDIT</a><br>
+										<a href="add-product.php" class="item_add delete">DELETE</a>
+										<div class="clearfix"> </div>
+								</div>
+								
+							</div>
+						</div>	
+					
+					<div class="clearfix"> </div>
+					</div>	
+				</div>
+				<div class="col-md-3 product-bottom">
+					<!--categories-->
+						<div class=" rsidebar span_1_of_left">
+								<h3 class="cate">Categories</h3>
+									 <ul class="menu-drop">
+									<li class="item1"><a href="#">Men </a>
+										<ul class="cute">
+											<li class="subitem1"><a href="men-shirts.php">Shirts</a></li>
+											<li class="subitem2"><a href="men-trousers.php">Trousers</a></li>
+											<li class="subitem3"><a href="men-shoes.php">Shoes</a></li>
+										</ul>
+									</li>
+									<li class="item2"><a href="#">Women </a>
+										<ul class="cute">
+											<li class="subitem1"><a href="women-trousers.php">Trousers </a></li>
+											<li class="subitem2"><a href="women-shoes.php">Shoes</a></li>
+											<li class="subitem3"><a href="women-skirts.php">Skirts</a></li>
+										</ul>
+									</li>
+									<li class="item3"><a href="#">Kids</a>
+										<ul class="cute">
+											<li class="subitem1"><a href="kids-shoes.php">Shoes</a></li>
+											<li class="subitem2"><a href="kids-trousers.php">Trousers</a></li>
+											<li class="subitem3"><a href="kids-shirts.php">Shirts</a></li>
+										</ul>
+									</li>
+								</ul>
+							</div>
+
+						<!--initiate accordion-->
+								<script type="text/javascript">
+									$(function() {
+									    var menu_ul = $('.menu-drop > li > ul'),
+									           menu_a  = $('.menu-drop > li > a');
+									    menu_ul.hide();
+									    menu_a.click(function(e) {
+									        e.preventDefault();
+									        if(!$(this).hasClass('active')) {
+									            menu_a.removeClass('active');
+									            menu_ul.filter(':visible').slideUp('normal');
+									            $(this).addClass('active').next().stop(true,true).slideDown('normal');
+									        } else {
+									            $(this).removeClass('active');
+									            $(this).next().stop(true,true).slideUp('normal');
+									        }
+									    });
+									
+									});
+								</script>
+
+					<a href="#" id="add-new">ADD NEW ITEM</a>
+
+		<!--//menu-->
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!--//content-->
+	</body>
+</html>
